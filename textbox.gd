f50d6a7b46d3d4aaf15084ab666866991eadb77e
Ghristian <44416068+Ghristian@users.noreply.github.com>
2022-09-06 17:50:46 +0200

    Create textbox.gd
    First version, some bugs while skipping the text.

---
+++ textbox.gd
@@ -0,0 +1,83 @@
+extends CanvasLayer
+
+const CHAR_READ_RATE = 0.05
+
+onready var textbox_container = $TextboxContainer
+onready var start_symbol = $TextboxContainer/MarginContainer/HBoxContainer/Start
+onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
+onready var label = $TextboxContainer/MarginContainer/HBoxContainer/Label
+
+enum State {
+	READY,
+	READING,
+	FINSIHED
+}
+
+var current_state = State.READY
+var text_queue = [ ]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Starting state = State.READY")
+	hide_textbox() 
+	queue_text("First long text queued up.  ")
+	queue_text("Second long text queued up.")
+	queue_text("Third long text queued up.  ")
+	queue_text("fourth long text queued up.  ")
+	queue_text("fourth long text queued up.  ")
+func _process(delta):
+	match current_state: 
+		State.READY:
+			if !text_queue.empty():
+				display_text()
+		State.READING:
+			if Input.is_action_just_pressed("ui_accept"):
+				label.percent_visible = 1.0
+				end_symbol.text = "v"
+				$Tween.remove_all()
+				change_state(State.FINISHED)
+		State.FINISHED:
+			if Input.is_action_just_pressed("ui_accept"):
+				change_state(State.READY)
+				hide_textbox()
+
+
+func queue_text(next_text):
+	text_queue.push_back(next_text)
+
+
+func hide_textbox():
+	start_symbol.text = ""
+	end_symbol.text = ""
+	label.text = ""
+	textbox_container.hide()
+
+func show_textbox():
+	start_symbol.text = "*"
+	textbox_container.show()
+
+
+func display_text():
+	var next_text = text_queue.pop_front()
+	label.text = next_text 
+	label.percent_visible = 0.0
+	change_state(State.READING)
+	show_textbox()
+	$Tween.interpolate_property(label, "percent_visible", 0.0, 1.0, len(next_text) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+	
+func _on_Tween_tween_completed():
+	end_symbol.text = "v"
+	change_state(State.FINSIHED)
+	
+
+func change_state(next_state):
+	current_state = next_state
+	match current_state:
+		State.READY:
+			print("Changing state to State.READY")
+		State.READING:
+			print("Changing state to State.READING")
+		State.FINISHED:
+			print("Changing state to State.FINISHED")
+
+
